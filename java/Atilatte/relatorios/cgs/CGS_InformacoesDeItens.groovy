fbcbe91f5671e4a5f8252623fea91ad5930c01c9
Leonardo Ledo <leonardoledo169@gmail.com>
2025-10-09 17:35:25 -0300

    Commit servlet Silcon

--- java/Atilatte/relatorios/cgs/CGS_InformacoesDeItens.groovy
+++ java/Atilatte/relatorios/cgs/CGS_InformacoesDeItens.groovy
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import java.util.Map;
 import java.util.HashMap;
 
-public class CGS_InformacoesDeidItens extends RelatorioBase {
+public class CGS_InformacoesDeItens extends RelatorioBase {
     @Override
     public String getNomeTarefa() {
         return "CGS - Informações De Itens";
