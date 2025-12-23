661ed8719ff19ff70b3b336364b849624d45caa6
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2025-12-23 09:19:02 -0300

    Alterado regra de aplicação de desconto considerando o campo customizado no cadastro do usuário

--- java/Silcon/formulas/spv/SPV_PreVendaItem.groovy
+++ java/Silcon/formulas/spv/SPV_PreVendaItem.groovy
@@ -40,22 +40,43 @@ class SPV_PreVendaItem extends FormulaBase {
 		abe30 = ccb01.ccb01cp;
 		
 		abm01 = ccb0101.ccb0101item;
+
+        Aab10 user = obterUsuarioLogado();
+        TableMap camposCustomUser = buscarCamposCustomizadosUser(user.aab10id);
+
 		//Calcula somente o % Desconto e TotalDoc caso tenha sido digitado o Valor Desconto
 		if(campoDigitado == 4) {
-			verificarLimMaxDesconto() // Verifica se o desconto é maior que o permitido por usuário
 			def percDesc = 0;
 			if(ccb0101.ccb0101total_Zero > 0 && ccb0101.ccb0101desc_Zero > 0) {
 				if(ccb0101.ccb0101desc_Zero > ccb0101.ccb0101total_Zero) {
 					throw new ValidacaoException("Valor do desconto não pode ser maior que o total do item.");
 				}
-				
-				percDesc = (ccb0101.ccb0101desc_Zero * 100) / ccb0101.ccb0101total_Zero;
 			}
-			ccb0101.ccb0101percDesc = percDesc;
-			ccb0101.ccb0101percDesc = round(ccb0101.ccb0101percDesc_Zero, 2);
-			
-			ccb0101.ccb0101totDoc = ccb0101.ccb0101total_Zero - ccb0101.ccb0101desc_Zero;
-			
+
+            if(camposCustomUser.getTableMap("aab10camposCustom") == null || camposCustomUser.getTableMap("aab10camposCustom").getInteger("aplic_desconto") == null) interromper("Necessário preencher o campo customizado de aplicação de desconto no cadastro do usuário logado.")
+
+            if(camposCustomUser.getTableMap("aab10camposCustom").getInteger("aplic_desconto") == 0){ // Aplica desconto no unitário
+
+                percDesc = (ccb0101.ccb0101desc_Zero * 100) / ccb0101.ccb0101unit_Zero;
+
+                // Unitário com desconto
+                ccb0101.ccb0101unit = ccb0101.ccb0101unit_Zero - ccb0101.ccb0101desc_Zero;
+
+                // Total do item com desconto
+                ccb0101.ccb0101total = round(ccb0101.ccb0101qtComl_Zero * ccb0101.ccb0101unit_Zero, 2);
+
+                // Total do documento com desconto
+                ccb0101.ccb0101totDoc = ccb0101.ccb0101total;
+
+            }else{
+                percDesc = (ccb0101.ccb0101desc_Zero * 100) / ccb0101.ccb0101total_Zero;
+
+                ccb0101.ccb0101totDoc = ccb0101.ccb0101total_Zero - ccb0101.ccb0101desc_Zero;
+            }
+
+            ccb0101.ccb0101percDesc = percDesc;
+            ccb0101.ccb0101percDesc = round(ccb0101.ccb0101percDesc_Zero, 2);
+
 			return;
 		}
 		
@@ -67,23 +88,27 @@ class SPV_PreVendaItem extends FormulaBase {
 		ccb0101.ccb0101total = ccb0101.ccb0101qtComl_Zero * ccb0101.ccb0101unit_Zero;
 		ccb0101.ccb0101total = round(ccb0101.ccb0101total_Zero, 2);
 		
-		//Calculando o valor do desconto com base no % Desconto
-		def valorDesc = 0;
-		if(ccb0101.ccb0101total_Zero > 0 && ccb0101.ccb0101percDesc_Zero > 0) {
-			if(ccb0101.ccb0101percDesc_Zero > 100) {
-				throw new ValidacaoException("% do desconto não pode ser maior que 100,00.");
-			}
-			
-			valorDesc = (ccb0101.ccb0101total_Zero * ccb0101.ccb0101percDesc_Zero) / 100;
-		}
-		ccb0101.ccb0101desc = valorDesc;
-		ccb0101.ccb0101desc = round(ccb0101.ccb0101desc_Zero, 2);
-		
+//		//Calculando o valor do desconto com base no % Desconto
+//		def valorDesc = 0;
+//		if(ccb0101.ccb0101total_Zero > 0 && ccb0101.ccb0101percDesc_Zero > 0) {
+//			if(ccb0101.ccb0101percDesc_Zero > 100) {
+//				throw new ValidacaoException("% do desconto não pode ser maior que 100,00.");
+//			}
+//
+//			valorDesc = (ccb0101.ccb0101total_Zero * ccb0101.ccb0101percDesc_Zero) / 100;
+//		}
+//		ccb0101.ccb0101desc = valorDesc;
+//		ccb0101.ccb0101desc = round(ccb0101.ccb0101desc_Zero, 2);
+//
 		//Calculando o TotalDoc
 		ccb0101.ccb0101totDoc = ccb0101.ccb0101total_Zero - ccb0101.ccb0101desc_Zero;
 	}
 
+    private TableMap buscarCamposCustomizadosUser(Long idUser){
+        String sql = "SELECT aab10camposCustom FROM aab10 WHERE aab10id = :idUser";
 
+        return getAcessoAoBanco().buscarUnicoTableMap(sql, Parametro.criar("idUser", idUser));
+    }
 	private void verificarLimMaxDesconto(){
 		if(abe40 == null) return;
 
