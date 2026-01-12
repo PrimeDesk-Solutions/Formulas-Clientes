6514e467d0056a6959a48a1fbdf67f2e057f6e8f
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-01-12 06:58:10 -0300

    Corrigido TAG EXTIPI da fórmula NFe_Reforma.groovy

--- java/Atilatte/formulas/srf/nfe/NFe_Reforma.groovy
+++ java/Atilatte/formulas/srf/nfe/NFe_Reforma.groovy
@@ -928,7 +928,7 @@ class NFe_Reforma extends FormulaBase {
             prod.addNode("cBenef", null, false);
             prod.addNode("tpCredPresIBSZFM", eaa0103.eaa0103credPresIbsZFM, false);
             if(abm01.abm01tipo != 2) {
-                prod.addNode("EXTIPI", ncm == null ? null : ncm.indexOf("/") == -1 ? null : ncm.substring(ncm.indexOf("/") +1), false);
+                prod.addNode("EXTIPI", ncm == null ? null : abg01.abg01codigo.indexOf("/") == -1 ? null : abg01.abg01codigo.substring(abg01.abg01codigo.indexOf("/") + 1,abg01.abg01codigo.length()), false);
             }
 
             Aaj15 aaj45 = getAcessoAoBanco().buscarRegistroUnicoById("Aaj15", eaa0103.eaa0103cfop.aaj15id);
