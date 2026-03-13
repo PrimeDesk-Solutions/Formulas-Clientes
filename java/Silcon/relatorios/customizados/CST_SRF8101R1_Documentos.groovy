f366373d3df2b70c7f94eb1332c4690228c8f529
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-03-12 23:49:12 -0300

    Alterado relatório CST_SRF8101R1_Documentos.groovy para considerar NFCe atrelado aos pedidos SILCON

--- java/Silcon/relatorios/customizados/CST_SRF8101R1_Documentos.groovy
+++ java/Silcon/relatorios/customizados/CST_SRF8101R1_Documentos.groovy
@@ -1,5 +1,6 @@
 package Silcon.relatorios.customizados
 
+import br.com.multiorm.ColumnType
 import br.com.multitec.utils.Utils
 import br.com.multitec.utils.collections.TableMap
 import sam.model.entities.ea.Eaa01;
@@ -90,67 +91,41 @@ public class CST_SRF8101R1_Documentos extends RelatorioBase {
         TableMap valoresTotais = new TableMap();
 
         for (dado in dados) {
+            Long idCentral = dado.getLong("abb01id");
             String descrSituacao = dado.getString("descrSituacao");
             LocalDate dtCancelamento = dado.getDate("dtCancelamento");
+            String docDerivado = buscarDesdobramentosDocumento(idCentral);
 
             if(descrSituacao == null || descrSituacao.isEmpty() && dtCancelamento == null){
                 dado.put("descrSituacao", "CANCELADO");
             }
 
-            if (idControle == null) {
-                dadosTmp.putAll(dado);
-                idControle = dado.getLong("eaa01id");
-                somarValores(dado, valoresTotais, campos);
-            } else if (idControle == dado.getLong("eaa01id")) {
-                somarValores(dado, valoresTotais, campos);
-            } else {
-                TableMap tmp = new TableMap();
-                tmp.putAll(dadosTmp);
-                tmp.putAll(valoresTotais);
-                comporValores(tmp, campos);
-                dadosRelatorio.add(tmp);
-
-                dadosTmp = new TableMap();
-                dadosTmp.putAll(dado);
-                valoresTotais = new TableMap()
-                idControle = dado.getLong("eaa01id");
-                somarValores(dado, valoresTotais, campos);
-            }
+            dado.put("docDerivado", docDerivado);
+            dado.put("nomeCampo1", "Total Doc.")
+            dado.put("nomeCampo2", "Total Financ.")
+            dado.put("valorCampo1", dado.getBigDecimal_Zero("eaa01totDoc"));
+            dado.put("valorCampo2", dado.getBigDecimal_Zero("eaa01totFinanc"));
         }
 
-        TableMap tmp = new TableMap();
-        tmp.putAll(dadosTmp);
-        tmp.putAll(valoresTotais);
-        comporValores(tmp, campos);
+        if(impressao == 1) return gerarXLSX("CST_SRF8101R1_Documentos_Excel", dados);
 
-        String descrSituacao = tmp.getString("descrSituacao");
-        LocalDate dtCancelamento = tmp.getDate("dtCancelamento");
-
-        if(descrSituacao == null || descrSituacao.isEmpty() && dtCancelamento == null){
-            tmp.put("descrSituacao", "CANCELADO");
-        }
-
-        dadosRelatorio.add(tmp);
-
-        if(impressao == 1) return gerarXLSX("CST_SRF8101R1_Documentos_Excel", dadosRelatorio);
-
-        return gerarPDF("CST_SRF8101R1_Documentos_PDF", dadosRelatorio)
+        return gerarPDF("CST_SRF8101R1_Documentos_PDF", dados)
 
     }
 
     private List<TableMap> buscarDadosRelatorio(Integer operacao, Integer numeroInicial, Integer numeroFinal, List<Long> idsEntidades, List<Long> idsTiposDoc, LocalDate[] dtEntradaSaida, LocalDate[] dtEmissao, List<Long> idsPcds, List<Long> idsEstados, List<Long> idsMunicipios){
-        String whereNumDoc = " WHERE abb01Nota.abb01num BETWEEN :numDocIni AND :numDocFin ";
-        String whereOperacao = operacao == 0 ? " AND eaa01Nota.eaa01esMov = 0 " : " AND eaa01Nota.eaa01esMov = 1";
-        String whereClassDoc = " AND eaa01Nota.eaa01clasDoc = " + Eaa01.CLASDOC_SRF;
+        String whereNumDoc = " WHERE abb01num BETWEEN :numDocIni AND :numDocFin ";
+        String whereOperacao = operacao == 0 ? " AND eaa01esMov = 0 " : " AND eaa01esMov = 1 ";
+        String whereClassDoc = " AND eaa01clasDoc = " + Eaa01.CLASDOC_SRF + " ";
         //String whereCancelamento = " AND eaa01Nota.eaa01cancData IS NULL ";
         String whereEntidades = idsEntidades != null && idsEntidades.size() > 0 ? " AND abe01id IN (:idsEntidades) " : "";
-        String whereTipoDoc = idsTiposDoc != null && idsTiposDoc.size() > 0 ? " AND aah01Nota.aah01id IN (:idsTiposDoc) " : "";
-        String whereDtEmissao = dtEmissao != null ? " AND abb01Nota.abb01data BETWEEN :dtEmissaoIni AND :dtEmissaoFin " : "";
-        String whereEsData = dtEntradaSaida != null ? " AND eaa01Nota.eaa01esData BETWEEN :dtEntradaSaidaIni AND :dtEntradaSaidaFin " : "";
+        String whereTipoDoc = idsTiposDoc != null && idsTiposDoc.size() > 0 ? " AND aah01id IN (:idsTiposDoc) " : "";
+        String whereDtEmissao = dtEmissao != null ? " AND abb01data BETWEEN :dtEmissaoIni AND :dtEmissaoFin " : "";
+        String whereEsData = dtEntradaSaida != null ? " AND eaa01esData BETWEEN :dtEntradaSaidaIni AND :dtEntradaSaidaFin " : "";
         String wherePcd = idsPcds != null && idsPcds.size() > 0 ? " AND abd01id IN (:idsPcds) " : "";
         String whereEstados = idsEstados != null && idsEstados.size() > 0 ? " AND aag02id IN (:idsEstados) " : "";
         String whereMunicipios = idsMunicipios != null && idsMunicipios.size() > 0 ? "AND aag0201id IN (:idsMunicipios) " : "";
-        String whereEmpresa = "AND eaa01Nota.eaa01gc = :idEmpresa ";
+        String whereEmpresa = "AND eaa01gc = :idEmpresa ";
 
         Parametro parametroNumDocIni = numeroInicial != null ? Parametro.criar("numDocIni", numeroInicial) : null;
         Parametro parametroNumDocFin = numeroFinal != null ? Parametro.criar("numDocFin", numeroFinal) : null;
@@ -165,103 +140,44 @@ public class CST_SRF8101R1_Documentos extends RelatorioBase {
         Parametro parametroMunicipios = idsMunicipios != null && idsMunicipios.size() > 0 ? Parametro.criar("idsMunicipios", idsMunicipios) : null;
         Parametro parametroEmpresa = Parametro.criar("idEmpresa", obterEmpresaAtiva().getAac10id());
 
-
-        String sql = " SELECT eaa01Nota.eaa01id AS eaa01id, aah01Nota.aah01codigo AS codTipoDocNota, abb01Nota.abb01num AS numDocNota, abb01Nota.abb01data AS dtEmissaoNota, " +
-                " eaa01Nota.eaa01esData AS dtEntradaSaidaNota, aaj03codigo AS codSituacao, aaj03descr AS descrSituacao, abe01codigo AS codEntidade, " +
-                " abe01na AS nomeEntidade, aag02uf AS ufEntidade, abe30codigo AS codCondPgto, abe30nome AS descrCondPgto, aah01Ref.aah01codigo AS codTipoDocRef, " +
-                " aah01Ref.aah01nome AS descrTipoDocRef, abb01Ref.abb01num AS numDocRef, eaa01nota.eaa01cancData AS dtCancelamento,  " +
-                " eaa0103Nota.eaa0103qtUso AS eaa0103qtuso, eaa0103Nota.eaa0103qtComl AS eaa0103qtcoml, eaa0103Nota.eaa0103unit AS eaa0103unit, " +
-                " eaa0103Nota.eaa0103total AS eaa0103total, eaa0103Nota.eaa0103totDoc AS eaa0103totdoc, eaa0103Nota.eaa0103totFinanc AS eaa0103totfinanc, eaa0103Nota.eaa0103json AS eaa0103json"+
-                " FROM eaa01 AS eaa01Nota " +
-                " INNER JOIN eaa0103 AS eaa0103Nota ON eaa0103Nota.eaa0103doc = eaa01Nota.eaa01id " +
-                " INNER JOIN abb01 AS abb01Nota ON abb01Nota.abb01id = eaa01Nota.eaa01central " +
-                " INNER JOIN abd01 ON abd01id = eaa01Nota.eaa01pcd "+
-                " INNER JOIN aah01 AS aah01Nota ON aah01Nota.aah01id = abb01Nota.abb01tipo " +
-                " LEFT JOIN aaj03 ON aaj03id = eaa01Nota.eaa01sitDoc " +
-                " INNER JOIN abe01 ON abe01id = abb01Nota.abb01ent  " +
-                " LEFT JOIN abe0101 ON abe0101ent = abe01id AND abe0101principal = 1 " +
-                " LEFT JOIN aag0201 ON aag0201id = abe0101municipio " +
-                " LEFT JOIN aag02 ON aag02id = aag0201uf " +
-                " LEFT JOIN abe30 ON abe30id = eaa01Nota.eaa01cp " +
-                " LEFT JOIN eaa01033 ON eaa01033item = eaa0103Nota.eaa0103id " +
-                " LEFT JOIN eaa0103 AS eaa0103ref ON eaa0103ref.eaa0103id = eaa01033itemDoc " +
-                " LEFT JOIN eaa01 AS eaa01ref ON eaa01ref.eaa01id = eaa0103ref.eaa0103doc " +
-                " LEFT JOIN abb01 AS abb01ref ON abb01ref.abb01id = eaa01ref.eaa01central " +
-                " LEFT JOIN aah01 AS aah01Ref On aah01Ref.aah01id = abb01ref.abb01tipo " +
-                whereNumDoc +
-                whereOperacao +
-                whereClassDoc +
-                //whereCancelamento +
-                whereEntidades +
-                whereTipoDoc +
-                whereDtEmissao +
-                whereEsData +
-                wherePcd +
-                whereEstados +
-                whereMunicipios +
-                whereEmpresa +
-                "ORDER BY abb01Nota.abb01num ";
+        String sql = " SELECT eaa01id, abb01id, aah01codigo AS codTipoDocNota, abb01num AS numDocNota, abb01data AS dtEmissaoNota, " +
+                        " eaa01esData AS dtEntradaSaidaNota, aaj03codigo AS codSituacao, aaj03descr AS descrSituacao, abe01codigo AS codEntidade, " +
+                        " abe01na AS nomeEntidade, aag02uf AS ufEntidade, abe30codigo AS codCondPgto, abe30nome AS descrCondPgto, aah01codigo AS codTipoDocRef, " +
+                        " eaa01cancData AS dtCancelamento, eaa01totDoc, eaa01totFinanc"+
+                        " FROM eaa01 " +
+                        " INNER JOIN abb01 ON abb01id = eaa01central " +
+                        " INNER JOIN abd01 ON abd01id = eaa01pcd "+
+                        " INNER JOIN aah01 ON aah01id = abb01tipo " +
+                        " LEFT JOIN aaj03 ON aaj03id = eaa01sitDoc " +
+                        " INNER JOIN abe01 ON abe01id = abb01ent  " +
+                        " LEFT JOIN abe0101 ON abe0101ent = abe01id AND abe0101principal = 1 " +
+                        " LEFT JOIN aag0201 ON aag0201id = abe0101municipio " +
+                        " LEFT JOIN aag02 ON aag02id = aag0201uf " +
+                        " LEFT JOIN abe30 ON abe30id = eaa01cp " +
+                        whereNumDoc +
+                        whereOperacao +
+                        whereClassDoc +
+                        whereEntidades +
+                        whereTipoDoc +
+                        whereDtEmissao +
+                        whereEsData +
+                        wherePcd +
+                        whereEstados +
+                        whereMunicipios +
+                        whereEmpresa +
+                        "ORDER BY abb01num ";
 
         return getAcessoAoBanco().buscarListaDeTableMap(sql, parametroNumDocIni, parametroNumDocFin, parametroEntidades, parametroTiposDoc, parametroDtEmissaoInicial, parametroDtEmissaoFinal, parametroDtEntSaidaInicial, parametroDtEntSaidaFinal, parametroPcds, parametroEstados, parametroMunicipios, parametroEmpresa )
     }
-    private void somarValores(TableMap valoresDocumento, TableMap valoresTotais, Map<String, String> campos) {
-        for (campo in campos) {
-            if (campo != null) {
-                String nomeCampo = buscarNomeCampoFixo(campo.value.toString()) != null ? buscarNomeCampoFixo(campo.value.toString()) : campo.value.toString();
-                if (valoresDocumento.getBigDecimal(nomeCampo) != null) {
-                    if (valoresTotais.getBigDecimal(nomeCampo) == null) {
-                        valoresTotais.put(nomeCampo, valoresDocumento.getBigDecimal_Zero(nomeCampo));
-                    } else {
-                        BigDecimal valorTotal = valoresTotais.getBigDecimal(nomeCampo) == null ? new BigDecimal(0) : valoresTotais.getBigDecimal(nomeCampo);
-                        BigDecimal valorItem = valoresDocumento.getBigDecimal(nomeCampo) == null ? new BigDecimal(0) : valoresDocumento.getBigDecimal(nomeCampo);
-                        BigDecimal soma = valorTotal + valorItem;
-                        valoresTotais.put(nomeCampo, soma)
-                    }
-                }
-            }
-        }
-    }
-    private void comporValores(TableMap dado, HashMap<String, String> campos) {
-        for (campo in campos) {
-            if (campo.value != null) {
-                String nomeCampo = buscarNomeCampoFixo(campo.value);
-                if (nomeCampo != null) {
-                    dado.put("nomeCampo" + campo.key, campo.value);
-                    dado.put("valorCampo" + campo.key, dado.getBigDecimal_Zero(nomeCampo))
-                } else {
-                    nomeCampo = buscarNomeCampoLivre(campo.value);
-                    dado.put("nomeCampo" + campo.key, nomeCampo);
-                    dado.put("valorCampo" + campo.key, dado.getBigDecimal_Zero(campo.value));
-                }
-            }
-        }
-    }
-    private String buscarNomeCampoFixo(String campo) {
-        switch (campo) {
-            case "Qtde. de Uso":
-                return "eaa0103qtuso"
-                break
-            case "Qtde. Comercial":
-                return "eaa0103qtcoml"
-                break
-            case "Preço Unitário":
-                return "eaa0103unit"
-                break
-            case "Total do Item":
-                return "eaa0103total"
-                break
-            case "Total Documento":
-                return "eaa0103totdoc"
-                break
-            case "Total Financeiro":
-                return "eaa0103totfinanc"
-                break
-        }
-    }
-    private String buscarNomeCampoLivre(String campo) {
-        def sql = " SELECT aah02descr FROM aah02 WHERE aah02nome = :nome "
-        return getAcessoAoBanco().obterString(sql, criarParametroSql("nome", campo))
-
+    private String buscarDesdobramentosDocumento(Long idCentral){
+        return getSession().createQuery("SELECT CONCAT(aah01codigo,' ', aah01nome, ' ', abb01num ) AS docDerivado " +
+                                        "FROM abb0102 " +
+                                        "INNER JOIN abb01 ON abb01id = abb0102doc " +
+                                        "INNER JOIN aah01 ON aah01id = abb01tipo " +
+                                        "WHERE abb0102central = :idCentral "+
+                                        "AND abb01tipo IN (295886, 584028, 35854674, 35854595)").setMaxResult(1)
+                                        .setParameter("idCentral", idCentral)
+                                        .getUniqueResult(ColumnType.STRING)
     }
 }
 //meta-sis-eyJkZXNjciI6IkNTVCAtIFNSRjgxMDFSMSBEb2N1bWVudG9zIiwidGlwbyI6InJlbGF0b3JpbyJ9
