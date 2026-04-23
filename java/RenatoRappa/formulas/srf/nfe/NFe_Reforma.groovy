3d552a2e0aa9623beb9edd5af8df364cf1d57ecf
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-04-23 13:27:08 -0300

    Acrescentado tag do cBenef na formula da NFE

--- java/RenatoRappa/formulas/srf/nfe/NFe_Reforma.groovy
+++ java/RenatoRappa/formulas/srf/nfe/NFe_Reforma.groovy
@@ -923,7 +923,7 @@ class NFe_Reforma extends FormulaBase {
                 prod.addNode("indEscala", eaa0103.eaa0103prodRelev == 0 ? null : eaa0103.eaa0103prodRelev == 1 ? "N" : "S", false);
                 prod.addNode("CNPJFab", StringUtils.extractNumbers(eaa0103.eaa0103cnpjFabr), false);
             }
-            prod.addNode("cBenef", null, false);
+            prod.addNode("cBenef", eaa0103.eaa0103codBenef, false);
             if(eaa0103.eaa0103credPresIbsZFM != 0) prod.addNode("tpCredPresIBSZFM", eaa0103.eaa0103credPresIbsZFM, false);
             if(abm01.abm01tipo != 2) {
                 prod.addNode("EXTIPI", ncm == null ? null : abg01.abg01codigo.indexOf("/") == -1 ? null : abg01.abg01codigo.substring(abg01.abg01codigo.indexOf("/") + 1,abg01.abg01codigo.length()), false);
