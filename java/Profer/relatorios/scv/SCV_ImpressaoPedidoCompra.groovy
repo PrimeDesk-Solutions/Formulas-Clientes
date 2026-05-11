387392515e26f5db63ef7065f0218b8cca080339
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-05-11 09:56:49 -0300

    Alterado relatório SCV_ImpressaoPedidoCompra.groovy para adequar ao novo layout

--- java/Profer/relatorios/scv/SCV_ImpressaoPedidoCompra.groovy
+++ java/Profer/relatorios/scv/SCV_ImpressaoPedidoCompra.groovy
@@ -76,23 +76,23 @@ public class SCV_ImpressaoPedidoCompra extends RelatorioBase {
         Parametro parametroDtEmissaoFin = emissao != null ? Parametro.criar("dtFinal", emissao[1]) : null;
 
         String sql =    "select distinct abb01num as numDoc, abe01nome as nomeEntidade, abe01ni as CNPJ, abe0101endereco as enderecoEntidade,  " +
-                        "abe0101numero as numeroEntidade, abe0101bairro as bairroEntidade, eaa0102pcNome as contato,  " +
-                        "abe0101ddd1 as DDDEntidade, abe0101fone1 as foneEntidade, aag0201nome as municipioEntidade, abb01data as dataDoc, eaa0103dtEntrega as dtEntrega, " +
-                        "eaa01totDoc as totDoc, abe30nome as condPgto, eaa01id " +
-                        "from eaa01 " +
-                        "inner join abb01 on eaa01central = abb01id " +
-                        "inner join abe01 on abb01ent = abe01id " +
-                        "left join abe0101 on abe0101ent = abe01id and abe0101principal = 1 " +
-                        "inner join eaa0102 on eaa0102doc = eaa01id " +
-                        "inner join aag0201 on abe0101municipio = aag0201id " +
-                        "inner join eaa0103 on eaa0103doc = eaa01id " +
-                        "left join abe30 on eaa01cp = abe30id " +
-                        whereEmpresa +
-                        whereTipos+
-                        whereNumDoc+
-                        whereEntidades+
-                        whereDtEmissao+
-                        whereClasDoc;
+                "abe0101numero as numeroEntidade, abe0101bairro as bairroEntidade, eaa0102pcNome as contato, eaa01totItens AS totItem,  " +
+                "abe0101ddd1 as DDDEntidade, abe0101fone1 as foneEntidade, aag0201nome as municipioEntidade, abb01data as dataDoc, eaa0103dtEntrega as dtEntrega, " +
+                "eaa01totDoc as totDoc, abe30nome as condPgto, eaa01id, abe01ie AS ieEntidade, abe0101email AS emailEntidade, abe01ni AS cnpj, abe01codigo AS codEntidade " +
+                "from eaa01 " +
+                "inner join abb01 on eaa01central = abb01id " +
+                "inner join abe01 on abb01ent = abe01id " +
+                "left join abe0101 on abe0101ent = abe01id and abe0101principal = 1 " +
+                "inner join eaa0102 on eaa0102doc = eaa01id " +
+                "inner join aag0201 on abe0101municipio = aag0201id " +
+                "inner join eaa0103 on eaa0103doc = eaa01id " +
+                "left join abe30 on eaa01cp = abe30id " +
+                whereEmpresa +
+                whereTipos+
+                whereNumDoc+
+                whereEntidades+
+                whereDtEmissao+
+                whereClasDoc;
         return getAcessoAoBanco().buscarListaDeTableMap(sql, parametroEmpresa, parametroTipos, parametroNumIni, parametroNumFin, parametroEntidades, parametroDtEmissaoIni, parametroDtEmissaoFin )
 
     }
@@ -100,26 +100,25 @@ public class SCV_ImpressaoPedidoCompra extends RelatorioBase {
     private List<TableMap> buscarDadosItens(Long eaa01id){
 
         String sql = "select eaa0103seq, abm01descr as descrItem, eaa0103qtUso as qtd, abm01pesoBruto as pesoBrutoItem,  " +
-                        "cast(eaa0103json ->> 'peso_bruto' as numeric(18,6)) as pesoBrutoDoc, eaa0103pcNum, abm01umu as umu, eaa0103unit as unit, eaa0103total as totalItem, " +
-                        "cast(eaa0103json ->> 'icms' as numeric(18,6)) as icms, " +
-                        "cast(eaa0103json ->> 'ipi' as numeric(18,6)) as ipi, eaa0103item " +
-                        "from eaa0103 " +
-                        "inner join abm01 on eaa0103item = abm01id " +
-                        "inner join aam06 on abm01umu = aam06id " +
-                        "where eaa0103doc = :idDoc "+
-                        "order by eaa0103seq"
+                "cast(eaa0103json ->> 'peso_bruto' as numeric(18,6)) as pesoBrutoDoc, eaa0103pcNum, aam06codigo as umu, eaa0103unit as unit, eaa0103total as totalItem, " +
+                "cast(eaa0103json ->> 'aliq_icms' as numeric(18,6)) as icms, " +
+                "cast(eaa0103json ->> 'aliq_ipi' as numeric(18,6)) as ipi, eaa0103item " +
+                "from eaa0103 " +
+                "inner join abm01 on eaa0103item = abm01id " +
+                "inner join aam06 on abm01umu = aam06id " +
+                "where eaa0103doc = :idDoc "+
+                "order by eaa0103seq"
 
         return getAcessoAoBanco().buscarListaDeTableMap(sql, Parametro.criar("idDoc", eaa01id ));
     }
 
     private BigDecimal buscarSaldoAtualItem(Long idItem){
         String sql = "select bcc02qt from bcc02 " +
-                        "where bcc02item =  :idItem " +
-                        "and bcc02status = 4224";
+                "where bcc02item =  :idItem " +
+                "and bcc02status = 4224";
 
         BigDecimal saldo =  getAcessoAoBanco().obterBigDecimal(sql, Parametro.criar("idItem", idItem))
 
         return saldo == null ? new BigDecimal(0) : saldo;
     }
 }
-//meta-sis-eyJkZXNjciI6IlNDViAtIEltcHJlc3PDo28gUGVkaWRvIENvbXByYSIsInRpcG8iOiJyZWxhdG9yaW8ifQ==
