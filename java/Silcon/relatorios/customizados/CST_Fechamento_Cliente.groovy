f2d7ecf431b6f340ce19269c4f0935843530af66
PrimeDesk-Solutions <leonardosuporte2@outlook.com>
2026-08-14 09:00:37 -0300

    Alterado relatório para considerar entidades que não possuem documentos financeiro e possuem cashcbak em aberto

--- java/Silcon/relatorios/customizados/CST_Fechamento_Cliente.groovy
+++ java/Silcon/relatorios/customizados/CST_Fechamento_Cliente.groovy
@@ -20,9 +20,10 @@ public class CST_Fechamento_Cliente extends RelatorioBase {
     }
     @Override
     public DadosParaDownload executar() {
+
         LocalDate[] dtEmissao = getIntervaloDatas("dtEmissao")
         LocalDate[] dtVencimento = getIntervaloDatas("dtVencimento");
-        List<Long> entidades = getListLong("entidades");
+        Long entidade = getLong("entidade");
 
         // Dados vindo da tela SCF0501
         Long abe01id = getLong("abe01id");
@@ -31,93 +32,90 @@ public class CST_Fechamento_Cliente extends RelatorioBase {
         LocalDate dtVctoIni = getLong("abe01id") != null && getString("dtVctoIni") != "null" ? LocalDate.parse(getString("dtVctoIni")) : null;
         LocalDate dtVctoFin = getLong("abe01id") != null && getString("dtVctoFin") != "null" ? LocalDate.parse(getString("dtVctoFin")) : null;
 
-        List<TableMap> docsFinanc = new ArrayList();
+        List<TableMap> registros = new ArrayList();
 
         if(abe01id == null){
-            docsFinanc = buscarDocumentosFinanceiros(dtEmissao, dtVencimento, entidades);
+            registros = buscarDocumentosFinanceiros(dtEmissao, dtVencimento, entidade);
         }else{
-            docsFinanc = buscarDocumentosFinanceirosPorIdEntidade(abe01id, dtEmisIni, dtEmisFin, dtVctoIni, dtVctoFin);
+            registros = buscarDocumentosFinanceirosPorIdEntidade(abe01id, dtEmisIni, dtEmisFin, dtVctoIni, dtVctoFin);
         }
 
-        Long idEntidade = null;
-        TableMap vlrVale = new TableMap();
-        for(doc in docsFinanc){
-
-            if(idEntidade != doc.getLong("idEnt")){
-                vlrVale = new TableMap();
-                idEntidade = doc.getLong("idEnt");
-                vlrVale = buscarValorValeCliente(idEntidade) == null ? new TableMap() : buscarValorValeCliente(idEntidade);
-            }
-
-            doc.put("valorVale", vlrVale.getBigDecimal_Zero("dad01saldo"));
-            doc.put("dataVale", vlrVale.getDate("dad01dtI"));
-
-            idEntidade = doc.getLong("idEnt");
+        if(registros == null || registros.size() == 0){ // Entidades sem financeiro em aberto
+            registros = entidade == null ? buscarInformacoesCliente(abe01id) : buscarInformacoesCliente(entidade);
         }
 
-        params.put("USUARIO", obterUsuarioLogado().getAab10user())
+        for(registro in registros){
+            Long idEntidade = registro.getLong("idEnt");
+            BigDecimal totalVale = buscarValorValeCliente(idEntidade)
 
-        return gerarPDF("CST_Fechamento_Cliente_PDF", docsFinanc);
+            registro.put("valorVale", totalVale);
+        }
+
+        params.put("USUARIO", obterUsuarioLogado().getAab10user());
+
+
+        return gerarPDF("CST_Fechamento_Cliente_PDF", registros);
 
     }
-    private List<TableMap> buscarDocumentosFinanceiros(LocalDate[] dtEmissao,LocalDate[] dtVencimento, List<Long> entidades){
+    private List<TableMap> buscarDocumentosFinanceiros(LocalDate[] dtEmissao,LocalDate[] dtVencimento, Long entidade){
 
+        if(entidade == null) interromper("Necessário atualizar a página do relatório.");
         String whereQuita = " WHERE abb01quita = 0 ";
         String whereRP = " AND daa01rp = 0 ";
         String whereDtEmissao = dtEmissao != null ? "AND abb01data BETWEEN :dtEmissaoIni AND :dtEmissaoFin " : "";
         String whereDtVctoR = dtVencimento != null ? "AND daa01dtVctoR BETWEEN :dtVctoIni AND :dtVctoFin " : "";
-        String whereEntidades = entidades != null && entidades.size() > 0 ? "AND abe01id IN (:entidades) " : "";
+        String whereEntidades = entidade != null ? "AND abe01id = :entidade " : "";
 
 
         Parametro parametroDtEmissaoIni = dtEmissao != null ? Parametro.criar("dtEmissaoIni", dtEmissao[0]) : null;
         Parametro parametroDtEmissaoFin = dtEmissao != null ? Parametro.criar("dtEmissaoFin", dtEmissao[1]) : null;
         Parametro parametroDtVctoIni = dtVencimento != null ? Parametro.criar("dtVctoIni", dtVencimento[0]) : null;
         Parametro parametroDtVctoFin = dtVencimento != null ? Parametro.criar("dtVctoFin", dtVencimento[1]) : null;
-        Parametro parametroEntidade = entidades != null && entidades.size() > 0 ? Parametro.criar("entidades", entidades) : null;
+        Parametro parametroEntidade = entidade != null  ? Parametro.criar("entidade", entidade) : null;
 
 
         String sql = "SELECT  " +
-                    "    aah01codigo AS codTipoDoc,  " +
-                    "    aah01nome AS descrTipoDoc,  " +
-                    "    aac10id AS empresa,   " +
-                    "    abe01codigo AS codEntidade,  " +
-                    "    abe01na AS naEntidade,  " +
-                    "    abe01id AS idEnt,  " +
-                    "    abb01num AS numDoc,  " +
-                    "    abb01parcela AS parcela,  " +
-                    "    daa01dtLcto AS dtLcto,  " +
-                    "    daa01dtVctoR AS dtVctoR,  " +
-                    "    abb01valor AS valor,   " +
-                    "    CASE  " +
-                    "        WHEN NOW() > daa01dtVctoR  " +
-                    "        THEN (CAST(daa01json ->> 'juros' AS numeric(18,6)) * CAST(TO_CHAR((NOW()) - (daa01dtVctoR),'DD') AS INT))  " +
-                    "        ELSE 0.00  " +
-                    "    END AS juros,  " +
-                    "    (abb01valor +  " +
-                    "        (CASE  " +
-                    "            WHEN NOW() > daa01dtVctoR  " +
-                    "            THEN (CAST(daa01json ->> 'juros' AS numeric(18,6)) * CAST(TO_CHAR((NOW()) - (daa01dtVctoR),'DD') AS INT))  " +
-                    "            ELSE 0.00  " +
-                    "        END) " +
-                    "    ) AS vlrFinal,  " +
-                    "    daa01obs AS observacao,  " +
-                    "    abf15codigo AS codPort,  " +
-                    "    abf15nome AS nomePort,  " +
-                    "    abf16codigo AS codOper,  " +
-                    "    abf16nome AS nomeOper  " +
-                    " FROM daa01  " +
-                    " INNER JOIN abb01 ON abb01id = daa01central  " +
-                    " INNER JOIN aah01 ON aah01id = abb01tipo  " +
-                    " INNER JOIN aac10 ON aac10id = daa01gc  " +
-                    " INNER JOIN abe01 ON abe01id = abb01ent  " +
-                    " LEFT JOIN abf15 ON abf15id = daa01port  " +
-                    " LEFT JOIN abf16 ON abf16id = daa01oper   " +
-                    whereQuita +
-                    whereRP+
-                    whereDtEmissao +
-                    whereDtVctoR +
-                    whereEntidades +
-                    "ORDER BY abe01codigo, daa01dtVctoR, abb01num, abb01parcela ";
+                "    aah01codigo AS codTipoDoc,  " +
+                "    aah01nome AS descrTipoDoc,  " +
+                "    aac10id AS empresa,   " +
+                "    abe01codigo AS codEntidade,  " +
+                "    abe01na AS naEntidade,  " +
+                "    abe01id AS idEnt,  " +
+                "    abb01num AS numDoc,  " +
+                "    abb01parcela AS parcela,  " +
+                "    daa01dtLcto AS dtLcto,  " +
+                "    daa01dtVctoR AS dtVctoR,  " +
+                "    abb01valor AS valor,   " +
+                "    CASE  " +
+                "        WHEN NOW() > daa01dtVctoR  " +
+                "        THEN (CAST(daa01json ->> 'juros' AS numeric(18,6)) * CAST(TO_CHAR((NOW()) - (daa01dtVctoR),'DD') AS INT))  " +
+                "        ELSE 0.00  " +
+                "    END AS juros,  " +
+                "    (abb01valor +  " +
+                "        (CASE  " +
+                "            WHEN NOW() > daa01dtVctoR  " +
+                "            THEN (CAST(daa01json ->> 'juros' AS numeric(18,6)) * CAST(TO_CHAR((NOW()) - (daa01dtVctoR),'DD') AS INT))  " +
+                "            ELSE 0.00  " +
+                "        END) " +
+                "    ) AS vlrFinal,  " +
+                "    daa01obs AS observacao,  " +
+                "    abf15codigo AS codPort,  " +
+                "    abf15nome AS nomePort,  " +
+                "    abf16codigo AS codOper,  " +
+                "    abf16nome AS nomeOper  " +
+                " FROM daa01  " +
+                " INNER JOIN abb01 ON abb01id = daa01central  " +
+                " INNER JOIN aah01 ON aah01id = abb01tipo  " +
+                " INNER JOIN aac10 ON aac10id = daa01gc  " +
+                " INNER JOIN abe01 ON abe01id = abb01ent  " +
+                " LEFT JOIN abf15 ON abf15id = daa01port  " +
+                " LEFT JOIN abf16 ON abf16id = daa01oper   " +
+                whereQuita +
+                whereRP+
+                whereDtEmissao +
+                whereDtVctoR +
+                whereEntidades +
+                "ORDER BY abe01codigo, daa01dtVctoR, abb01num, abb01parcela ";
 
         return getAcessoAoBanco().buscarListaDeTableMap(sql, parametroDtEmissaoIni, parametroDtEmissaoFin, parametroDtVctoIni, parametroDtVctoFin, parametroEntidade);
     }
@@ -126,12 +124,12 @@ public class CST_Fechamento_Cliente extends RelatorioBase {
         String whereQuita = " WHERE abb01quita = 0 ";
         String whereRP = " AND daa01rp = 0 ";
         String whereDtEmissao = dtEmisIni != null && dtEmisFin != null ? "AND abb01data BETWEEN :dtEmissaoIni AND :dtEmissaoFin " :
-                                dtEmisIni != null && dtEmisFin == null ? "AND abb01data >= :dtEmissaoIni " :
-                                dtEmisIni == null && dtEmisFin != null ? "AND abb01data <= :dtEmissaoFin" : "";
+                dtEmisIni != null && dtEmisFin == null ? "AND abb01data >= :dtEmissaoIni " :
+                        dtEmisIni == null && dtEmisFin != null ? "AND abb01data <= :dtEmissaoFin" : "";
 
         String whereDtVctoR = dtVctoIni != null && dtVctoFin != null ? "AND daa01dtVctoR BETWEEN :dtVctoIni AND :dtVctoFin " :
-                              dtVctoIni != null && dtVctoFin == null ? "AND daa01dtVctoR >= :dtVctoIni " :
-                              dtVctoIni == null && dtVctoFin != null ? "AND daa01dtVctoR <= :dtVctoFin " : "";
+                dtVctoIni != null && dtVctoFin == null ? "AND daa01dtVctoR >= :dtVctoIni " :
+                        dtVctoIni == null && dtVctoFin != null ? "AND daa01dtVctoR <= :dtVctoFin " : "";
 
         String whereEntidade = "AND abe01id = :abe01id "
 
@@ -188,19 +186,24 @@ public class CST_Fechamento_Cliente extends RelatorioBase {
 
         return getAcessoAoBanco().buscarListaDeTableMap(sql, parametroDtEmissaoIni, parametroDtEmissaoFin, parametroDtVctoIni, parametroDtVctoFin, parametroEntidade);
     }
-    private TableMap buscarValorValeCliente(Long idEntidade){
+    private BigDecimal buscarValorValeCliente(Long idEntidade){
         Long idEmpresa = obterEmpresaAtiva().getAac10id();
 
         String sql = "SELECT COALESCE(SUM(dad01saldo), 0.00) AS dad01saldo " +
                 " FROM dad01 " +
                 " WHERE dad01ent = :idEntidade " +
                 " AND dad01tipo = 35610614 "
-                //" AND dad01eg = :idEmpresa ";
+        //" AND dad01eg = :idEmpresa ";
 
         Parametro parametroEntidade = Parametro.criar("idEntidade", idEntidade);
         Parametro parametroEmpresa = Parametro.criar("idEmpresa", idEmpresa)
 
-        return getAcessoAoBanco().buscarUnicoTableMap(sql, parametroEntidade, parametroEmpresa);
+        return getAcessoAoBanco().obterBigDecimal(sql, parametroEntidade, parametroEmpresa);
+    }
+    private List<TableMap> buscarInformacoesCliente(Long entidade){
+        String sql = "SELECT abe01id AS idEnt, abe01codigo AS codEntidade , abe01nome AS naEntidade FROM abe01 WHERE abe01id = :entidade ";
+
+        return getAcessoAoBanco().buscarListaDeTableMap(sql, Parametro.criar("entidade", entidade));
     }
 }
 //meta-sis-eyJkZXNjciI6IkNTVCAtIEZlY2hhbWVudG8gQ2xpZW50ZSIsInRpcG8iOiJyZWxhdG9yaW8ifQ==
